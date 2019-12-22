71b992fee9190a2450d0d63e590ee823b1741a24
Timothy H <timh1203@gmail.com>
2019-12-22 17:05:21 -0600

    Add axios, proxy port, hello route

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    hello: null
+  }
+
+  componentDidMount() {
+    axios.get('/hello')
+      .then(res => this.setState({ hello: res.data }))
+      .catch(error => console.error(error))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.hello
+          ? <div>{this.state.hello}</div>
+          : ''}
+      </div>
+    )
+  }
 }
 
 export default App;
--- server/main/app.js
+++ server/main/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var indexRouter = require('./routes')
 
 var app = express();
 
@@ -11,5 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter)
 
 module.exports = app;

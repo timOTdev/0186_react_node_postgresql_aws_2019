4b9defb3c1d9875d9ac245ad261d84704780fce6
Timothy H <timh1203@gmail.com>
2019-12-23 21:16:55 -0600

    Add reducers and entry in index

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,5 +1,6 @@
 import Reducer1 from './reducer1'
 import AuthReducer from './auth_reducer';
+import PostsReducer from './posts_reducer';
 import UserReducer from './user_reducer';
 import { combineReducers } from 'redux';
 
@@ -7,6 +8,7 @@ import { combineReducers } from 'redux';
 const rootReducer = combineReducers({
   reducer1: Reducer1,
   auth_reducer: AuthReducer,
+  posts_reducer: PostsReducer,
   user_reducer: UserReducer
 })
 

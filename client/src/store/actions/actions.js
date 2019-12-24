750a3a73bdde294b00801674e197df1ccdb0bcb2
Timothy H <timh1203@gmail.com>
2019-12-23 21:06:27 -0600

    Add actions and types

--- client/src/store/actions/actions.js
+++ client/src/store/actions/actions.js
@@ -8,7 +8,6 @@ export const FAILURE = {
   type: ACTION_TYPES.FAILURE
 }
 
-
 export const success = () => {
   return {
     type: ACTION_TYPES.SUCCESS
@@ -28,7 +27,6 @@ export const user_input = (text) => {
   }
 }
 
-
 export const login_success = () => {
   return {
     type: ACTION_TYPES.LOGIN_SUCCESS
@@ -41,7 +39,6 @@ export const login_failure = () => {
   }
 }
 
-
 export const add_profile = (profile) => {
   return {
     type: ACTION_TYPES.ADD_PROFILE,
@@ -52,5 +49,44 @@ export const add_profile = (profile) => {
 export const remove_profile = () => {
   return {
     type: ACTION_TYPES.REMOVE_PROFILE
+  }
+}
+
+export const set_db_profile = (profile) => {
+  return {
+    type: ACTION_TYPES.SET_DB_PROFILE,
+    payload: profile
+  }
+}
+
+export const remove_db_profile = () => {
+  return {
+    type: ACTION_TYPES.REMOVE_DB_PROFILE
+  }
+}
+
+export const fetch_db_posts = (posts) => {
+  return {
+    type: ACTION_TYPES.FETCH_DB_POSTS,
+    payload: posts
+  }
+}
+
+export const remove_db_posts = () => {
+  return {
+    type: ACTION_TYPES.REMOVE_DB_POSTS
+  }
+}
+
+export const fetch_post_comments = (comments) => {
+  return {
+    type: ACTION_TYPES.FETCH_POST_COMMENTS,
+    payload: comments
+  }
+}
+
+export const remove_post_comments = () => {
+  return {
+    type: ACTION_TYPES.REMOVE_POST_COMMENTS
   }
 }
